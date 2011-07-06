f097b9206c2969f5ff94f9c74fbf42f46e2ab46b
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2011-07-06 15:36:49 +0200

    updated the test

--- compile.sh
+++ compile.sh
@@ -6,13 +6,13 @@ gcc -o 3 memcpy3.c
 gcc -o 4 memcpy4.c
 gcc -o 5 memcpy5.c
 
-@echo "Stevens algoritme"
+echo "Stevens algoritme"
 time ./1
-@echo "Simpel algoritme"
+echo "Simpel algoritme"
 time ./2
-@echo "Simpel Duffs device"
+echo "Simpel Duffs device"
 time ./3
-@echo "Complex Duffs device"
+echo "Complex Duffs device"
 time ./4
-@echo "Native library"
+echo "Native library"
 time ./5
